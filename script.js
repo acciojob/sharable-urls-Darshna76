29e74fa993b91a7edc18d6f3599d6522b2545ccd
Darshna76 <117654044+Darshna76@users.noreply.github.com>
2023-05-06 22:28:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// your code here
+  function changeText() {
+var name=document.getElementById("name").value;
+var year=document.getElementById("year").value;
+var h3 =document.getElementById("url");
+
+	 var queryString = "?name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
+	  
+	  
+		   h3.textContent = "https://localhost:8080/" + queryString;
+	  }
+}
